11c4e56b06f3c4eba5faec61cca0da46995c991b
Christopher Eagle <cee.eagle@gmail.com>
2025-01-15 21:36:46 +0000

    added exampke

---
+++ examples/hogwartsStudent.ts
@@ -0,0 +1,52 @@
+import Lassie from "../src/core/lassie.js"; // Adjust the path based on your project structure
+import * as v from "valibot";
+
+interface HogwartsStudent {
+  id: string;
+  name: string;
+  alternate_names: string[];
+  species: string;
+  gender: string;
+  house: string;
+  dateOfBirth: string;
+  yearOfBirth: number;
+  wizard: boolean;
+  ancestry: string;
+  eyeColour: string;
+  hairColour: string;
+  wand: {
+    wood: string;
+    core: string;
+    length: number;
+  };
+  patronus: string;
+  hogwartsStudent: boolean;
+  hogWartsStaff: boolean;
+  actor: string;
+  alternate_actors: string[];
+  alive: boolean;
+  image: string;
+}
+
+const isHogwartsStudent = <HogwartsStudent>(
+  data: unknown
+): data is HogwartsStudent => {
+  const schema = v.array(
+    v.object({
+      hogwartsStudent: v.pipe(v.boolean(), v.literal(true)),
+    })
+  );
+  return v.is(schema, data);
+};
+
+async function example() {
+  try {
+    const data = await Lassie<HogwartsStudent>(
+      "https://hp-api.onrender.com/api/character/9e3f7ce4-b9a7-4244-b709-dae5c1f1d4a8",
+      isHogwartsStudent
+    );
+    console.log("Received data:", data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
